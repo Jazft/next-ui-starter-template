82fd6938dcc88d78937bf53f61da7cc393a72c3b
Vilictus <imvilictus@gmail.com>
2023-05-13 15:52:03 +0000

    v.0.1

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,8 @@
 import Head from 'next/head'
 import React, { useState, useEffect } from 'react';
 import { AppProps } from 'next/app';
-import { NextUIProvider, createTheme } from "@nextui-org/react";
+import { NextUIProvider } from "@nextui-org/react";
 import "@/styles/globals.css";
-import loader from '@/pages/components/Loader'
 
 // ===== App: MyApp ==== //
 
@@ -14,23 +13,23 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   // ====== Loader Code ===== //
 
-  const [loading, setLoading] = useState(true);
+  // const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    window.addEventListener('load', () => {
-      setLoading(false);
-    });
-  }, []);
+  // useEffect(() => {
+  //   window.addEventListener('load', () => {
+  //     setLoading(false);
+  //   });
+  // }, []);
 
   // ====== Loader Code End  ===== //
 
   return (
     <NextUIProvider>
-      <Head>
-        <title>NextUI ⎯ Loaded</title>
-      </Head>
-      {loading && <loader />}
-      <Component  {...pageProps} />
+          <Head>
+            <title>NextUI ⎯ Loaded</title>
+          </Head>
+          {/* {loading && <loader />} */}
+         <Component  {...pageProps} />
     </NextUIProvider>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,7 @@
 // ===== General Exports ==== //
 
-import React from "react";
-import { Text, Row, Spacer, Loading, Container, Input } from "@nextui-org/react";
-import NavBar from '@/pages/components/Navbar'
-import Main from '@/pages/components/Loader'
+import React, { useState } from "react";
+import { Text, Container } from "@nextui-org/react";
 import { Poppins } from 'next/font/google'
 
 const pp = Poppins({
@@ -17,33 +15,9 @@ const pp = Poppins({
 export default function Home() {
   return (
     <>
-      <Row css={{ height: '100vh', flexDirection: 'column' }} justify="center" align="center">
-        <Text className={pp.className} color="$dark" h3>Google MetaPreview</Text>
-
-        <Container>
-          <Container sm>
-            <Row>
-              <Row css={{ flexDirection: 'column' }}>
-                <Text className="label" h6 b>Website URL:</Text>
-                <Input placeholder="www.example.com" color="primary" css={{ $$inputColor: "#eee", $$inputBorderColor: '#ccc' }} bordered borderWeight="light" style={{ color: '#000' }} />
-                <Spacer inline />
-                <Text className="label" h6 b>Title:</Text>
-                <Input placeholder="My Website" color="primary" css={{ $$inputColor: "#eee", $$inputBorderColor: '#ccc' }} bordered borderWeight="light" style={{ color: '#000' }} />
-                <Spacer inline />
-                <Text className="label" h6 b>Description:</Text>
-                <Input placeholder="This is my website description! Wow!" color="primary" css={{ $$inputColor: "#eee", $$inputBorderColor: '#ccc' }} bordered borderWeight="light" style={{ color: '#000' }} />
-              </Row>
-              <Row>
-                
-              </Row>
-
-            </Row>
-
-
-          </Container>
-        </Container>
-
-      </Row>
+     <Container css={{ p: '$10 $14' }}>
+        <Text className={pp.className}> <b>NextUI + NextJS:</b> Loaded</Text>
+     </Container>
     </>
   );
 }
