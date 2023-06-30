a990bd5b5ed2a8fdba5571c3aa09820ceb7cd30f
Syraxes/Vilictus <101947195+Vilictus@users.noreply.github.com>
2023-06-30 17:53:56 -0500

    main

--- config/siteMeta.ts
+++ config/siteMeta.ts
@@ -3,5 +3,5 @@ export type SiteConfig = typeof siteConfig;
 export const siteConfig = {
 	name: "Workspace",
 	description: "N/A.",
-	repo: 'https://github.com/Vilictus/nextui-starter-template'
+	repo: 'https://github.com/Vilictus/next-ui-starter-template'
 };
